33b514c66871146bbd0d8fc21e439969101c94f9
lerochka <76012407+ppnnh@users.noreply.github.com>
2022-03-02 12:25:09 +0300

    Add files via upload

---
+++ Laba_1/Laba_1/ICalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_1
+{
+    internal interface ICalculator
+    {
+        bool SetVarA(string varA);
+        bool SetVarB(string varB);
+        void SetOperation(string operation);
+        string ShowLastMemory();
+        string ShowNextMemory();
+        void Clean();
+        string Equal(string varB);
+    }
+}
